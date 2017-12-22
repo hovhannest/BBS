1b519663d7acda6f6db0534e0b4b94b2649bbd1f
norayr <norayr@arnet.am>
2017-12-22 20:34:30 +0400

    adding calc script, filled spaces with F grades.

---
+++ calc.sh
@@ -0,0 +1,60 @@
+tonum(){
+# grades mapping from https://en.wikipedia.org/wiki/Academic_grading_in_the_United_States#Numerical_and_Letter_Grades
+if [ "$1" == "A+" ]; then
+   return $(( 100 ))
+elif [ "$1" == "A" ]; then
+   return $(( 96 ))
+elif [ "$1" == "A-" ]; then
+   return $(( 92 ))
+elif [ "$1" == "B+" ]; then
+   return $(( 89 ))
+elif [ "$1" == "B" ]; then
+   return $(( 86 ))
+elif [ "$1" == "B-" ]; then
+   return $(( 82 ))
+elif [ "$1" == "C+" ]; then
+   return $(( 79 ))
+elif [ "$1" == "C" ]; then
+   return $(( 76 ))
+elif [ "$1" == "C-" ]; then
+   return $(( 72 ))
+elif [ "$1" == "D+" ]; then
+   return $(( 69 ))
+elif [ "$1" == "D" ]; then
+   return $(( 66 ))
+elif [ "$1" == "D-" ]; then
+   return $(( 62 ))
+elif [ "$1" == "F" ]; then
+   return $(( 59 ))
+else
+   return $(( 59 ))
+fi
+
+}
+
+add(){
+
+  sm=$(( $j + $i ))
+
+}
+
+while read line
+do
+  name=`echo $line | awk {' print $1 '}`
+  gr0=`echo $line | awk {' print $2 '}`
+  gr1=`echo $line | awk {' print $3 '}`
+  gr2=`echo $line | awk {' print $4 '}`
+  gr3=`echo $line | awk {' print $5 '}`
+
+  tonum $gr0
+  grnum0=$?
+  tonum $gr1
+  grnum1=$?
+  tonum $gr2
+  grnum2=$?
+  tonum $gr3
+  grnum3=$?
+  echo "$name $grnum0"
+
+
+done < grades
